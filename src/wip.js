6525e42d98857c77214b27fb0a7ffc3a9dce06fa
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-31 13:07:37 -0700

    feat(wip): add wip feature to tipe cat bot

---
+++ src/wip.js
@@ -0,0 +1,37 @@
+async function checkWIP(context) {
+  const { pull_request: pr } = context.payload
+  const currentStatus = await getCurrentStatus(context)
+  const isWip = containsWIP(pr.title)
+  const newStatus = isWip ? 'pending' : 'success'
+  // check to see if status has changed
+  const hasChanged = currentStatus !== newStatus
+
+  if (!hasChanged) {
+    return
+  }
+
+  const wipStatus = {
+    state: newStatus,
+    description: isWip ? 'work in progress' : 'ready for review'
+  }
+
+  return wipStatus
+}
+
+function containsWIP(title) {
+  return /\b(wip|do not merge|work in progress)\b/i.test(title)
+}
+
+async function getCurrentStatus(context) {
+  const {
+    data: { statuses }
+  } = await context.github.repos.getCombinedStatusForRef(
+    context.repo({
+      ref: context.payload.pull_request.head.sha
+    })
+  )
+
+  return (statuses.find(status => status.context === 'WIP') || {}).state
+}
+
+module.exports = checkWIP
