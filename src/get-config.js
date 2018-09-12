b14e2b1a17472d977995f232bb7ebfe13acb2548
Olivia Oddo <olivia.oddo@gmail.com>
2018-09-12 15:03:41 -0700

    refactor: move config logic to its own function so I can test that shit

---
+++ src/get-config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+async function getConfig(context){
+  const { pull_request: pr } = context.payload
+  const params = {
+    path: 'commitlint.config.js',
+    ref: pr.head.ref
+  }
+  let config;
+
+  // delete custom config file that may have been created by previous event
+  try {
+    fs.unlinkSync('src/commitlint-custom.config.js')
+  } catch(e){
+    context.log('Existing custom config not present')
+  }
+
+  // check if pr contains custom config file and write it to src
+  try {
+    const customConfig = await context.github.repos.getContent(context.repo(params))
+    let buff = Buffer.from(customConfig.data.content, 'base64')
+    fs.writeFileSync('src/commitlint-custom.config.js', buff)
+  } catch (e) {
+    context.log('Using default config')
+  }
+
+  if(fs.existsSync('src/commitlint-custom.config.js')){
+    config = require('./commitlint-custom.config.js')
+    if(config.extends){
+      context.log('Extends not supported, using default config.')
+      config = require('./commitlint.config.js')
+    }  
+  } else {
+    config = require('./commitlint.config.js')
+  }
+
+  return config;
+
+}
+
+module.exports = getConfig
--- src/lint.js
+++ src/lint.js
@@ -1,44 +1,13 @@
-const fs = require('fs')
 const { lint, load } = require('@commitlint/core')
+const getConfig = require('./get-config')
 
 // paginate all pr commits
 async function lintCommits(context) {
   const pull = context.issue()
-  const { pull_request: pr } = context.payload
   const { paginate, pullRequests} = context.github
-  const params = {
-    path: 'commitlint.config.js',
-    ref: pr.head.ref
-  }
   let lintStatus
-  let config;
-
-  // delete custom config file that may have been created by previous event
-  try {
-    fs.unlinkSync('src/commitlint-custom.config.js')
-  } catch(e){
-    context.log('Existing custom config not present')
-  }
-
-  // check if pr contains custom config file and write it to src
-  try {
-    const customConfig = await context.github.repos.getContent(context.repo(params))
-    let buff = Buffer.from(customConfig.data.content, 'base64')
-    fs.writeFileSync('src/commitlint-custom.config.js', buff)
-  } catch (e) {
-    context.log('Using default config')
-  }
-
-  if(fs.existsSync('src/commitlint-custom.config.js')){
-    config = require('./commitlint-custom.config.js')
-    if(config.extends){
-      context.log('Extends not supported, using default config.')
-      config = require('./commitlint.config.js')
-    }  
-  } else {
-    config = require('./commitlint.config.js')
-  }
-
+  const config = await getConfig(context);
+  console.log("CONFIZG", config)
   await paginate(pullRequests.getCommits(pull), async ({ data }) => {
     // create an empty summary
     const report = { valid: true, commits: [] }
