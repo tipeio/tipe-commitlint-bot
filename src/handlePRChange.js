3fc6c0c2d32f2ff46b7577bd4650cf1cc54eb9e7
Olivia Oddo <olivia.oddo@gmail.com>
2018-09-12 14:03:48 -0700

    refactor(config): refactor config, rewriting the default config was effecting proceeding pr events

--- src/handlePRChange.js
+++ src/handlePRChange.js
@@ -2,23 +2,9 @@ const lintCommits = require('./lint')
 const checkWIP = require('./wip')
 const format = require('./format')
 const createCheck = require('./createCheck')
-const fs = require('fs')
 
 async function handlePRChange(context) {
   const pr = context.payload.pull_request
-
-  const params = {
-    path: 'commitlint.config.js',
-    ref: pr.head.ref
-  }
-
-  try {
-    const config = await context.github.repos.getContent(context.repo(params))
-    let buff = Buffer.from(config.data.content, 'base64')
-    fs.writeFileSync('src/commitlint.config.js', buff)
-  } catch (e) {
-    console.log('using default config')
-  }
 
   const lintStatus = await lintCommits(context)
   const wipStatus = await checkWIP(context)
--- src/lint.js
+++ src/lint.js
@@ -1,16 +1,46 @@
+const fs = require('fs')
 const { lint, load } = require('@commitlint/core')
-
-const config = require('./commitlint.config.js')
 
 // paginate all pr commits
 async function lintCommits(context) {
   const pull = context.issue()
-  const { paginate, pullRequests } = context.github
+  const { pull_request: pr } = context.payload
+  const { paginate, pullRequests} = context.github
+  const params = {
+    path: 'commitlint.config.js',
+    ref: pr.head.ref
+  }
   let lintStatus
+  let config;
+
+  // delete custom config file that may have been created by previous event
+  try {
+    fs.unlinkSync('src/commitlint-custom.config.js')
+  } catch(e){
+    context.log('Existing custom config not present')
+  }
+
+  // check if pr contains custom config file and write it to src
+  try {
+    const customConfig = await context.github.repos.getContent(context.repo(params))
+    let buff = Buffer.from(customConfig.data.content, 'base64')
+    fs.writeFileSync('src/commitlint-custom.config.js', buff)
+  } catch (e) {
+    context.log('using default config')
+  }
+
+  try {
+    config = require('./commitlint-custom.config.js')
+
+  } catch (e) {
+    config = require('./commitlint.config.js')
+
+  }
 
   await paginate(pullRequests.getCommits(pull), async ({ data }) => {
     // create an empty summary
     const report = { valid: true, commits: [] }
+
     const { rules } = await load(config)
     // error and warning counters
     let errorCount = 0
