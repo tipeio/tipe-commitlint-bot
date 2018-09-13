24c120156a40e87519de923001ace73eddc08468
Olivia Oddo <olivia.oddo@gmail.com>
2018-09-13 13:56:04 -0700

    fix(test): fix tests to work with json config

--- test/get-config.test.js
+++ test/get-config.test.js
@@ -15,35 +15,38 @@ describe('Get Config', () => {
   test('should return custom config', async () => {
     const github = { github: {
       repos: {
-        getContent: jest.fn().mockResolvedValue({data: { content: 'bW9kdWxlLmV4cG9ydHMgPSB7fQ=='}})
+        getContent: jest.fn().mockResolvedValue({data: { content: 'ewogICJydWxlcyI6IHsgImhlYWRlci1tYXgtbGVuZ3RoIjogWzIsICJhbHdheXMiLCAyXX0KfQ=='}})
       }
     }}
     const config = await getConfig({...context, ...github})
     expect.assertions(2)
-    expect(config).toEqual(module.exports = {})
+    expect(config).toEqual({
+      "rules": { "header-max-length": [2, "always", 2]}
+    })
     expect(context.log.mock.calls.length).toBe(0);
   })
+  test('should return default config if extends exists', async () => {
+    const githubThree = { github: {
+      repos: {
+        getContent: jest.fn().mockResolvedValue({data: { content: 'ewogICJleHRlbmRzIjogIlsnQGNvbW1pdGxpbnQvQGFuZ3VhbHJdIgoKfQ=='}})
+      }
+    }}
+    context.log.mockReset();
+    const config = await getConfig({...context, ...githubThree})
+    expect.assertions(2)
+    expect(config).toEqual(require('../src/commitlint.config'))
+    expect(context.log.mock.calls[0][0]).toEqual('Extends not supported, using default config.')
+  })
   test('should return default config for no custom config', async () => {
-    const github = { github: {
+    const githubTwo = { github: {
       repos: {
         getContent: jest.fn().mockRejectedValue(404)
       }
     }}
     context.log.mockReset();
-    const config = await getConfig({...context, ...github})
+    const config = await getConfig({...context, ...githubTwo})
     expect.assertions(2)
     expect(config).toEqual(require('../src/commitlint.config'))
     expect(context.log.mock.calls[0][0]).toEqual('Using default config')
-  })
-  test('should return default config if extends exists', async () => {
-    const github = { github: {
-      repos: {
-        getContent: jest.fn().mockResolvedValue({data: { content: 'bW9kdWxlLmV4cG9ydHMgPSB7CmV4dGVuZHM6IFsnQGNvbW1pdGxpbnQvYW5ndWxhciddCn0='}})
-      }
-    }}
-    const config = await getConfig({...context, ...github})
-    expect.assertions(2)
-    expect(config).toEqual(require('../src/commitlint.config'))
-    expect(context.log.mock.calls[0][0]).toEqual('Extends not supported, using default config.')
   })
 })
