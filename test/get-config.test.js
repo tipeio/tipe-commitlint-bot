d60182e768153cd21b9facd0bd2c827abd80e456
Olivia Oddo <olivia.oddo@gmail.com>
2018-09-13 14:13:49 -0700

    fix: remove log tests from get config

--- test/get-config.test.js
+++ test/get-config.test.js
@@ -9,7 +9,6 @@ describe('Get Config', () => {
         }
       }
     },
-    log: jest.fn(),
     repo: jest.fn()
   }
   test('should return custom config', async () => {
@@ -19,11 +18,9 @@ describe('Get Config', () => {
       }
     }}
     const config = await getConfig({...context, ...github})
-    expect.assertions(2)
     expect(config).toEqual({
       "rules": { "header-max-length": [2, "always", 2]}
     })
-    expect(context.log.mock.calls.length).toBe(0);
   })
   test('should return default config if extends exists', async () => {
     const githubThree = { github: {
@@ -31,11 +28,8 @@ describe('Get Config', () => {
         getContent: jest.fn().mockResolvedValue({data: { content: 'ewogICJleHRlbmRzIjogIlsnQGNvbW1pdGxpbnQvQGFuZ3VhbHJdIgoKfQ=='}})
       }
     }}
-    context.log.mockReset();
     const config = await getConfig({...context, ...githubThree})
-    expect.assertions(2)
     expect(config).toEqual(require('../src/commitlint.config'))
-    expect(context.log.mock.calls[0][0]).toEqual('Extends not supported, using default config.')
   })
   test('should return default config for no custom config', async () => {
     const githubTwo = { github: {
@@ -43,10 +37,7 @@ describe('Get Config', () => {
         getContent: jest.fn().mockRejectedValue(404)
       }
     }}
-    context.log.mockReset();
     const config = await getConfig({...context, ...githubTwo})
-    expect.assertions(2)
     expect(config).toEqual(require('../src/commitlint.config'))
-    expect(context.log.mock.calls[0][0]).toEqual('Using default config')
   })
 })
